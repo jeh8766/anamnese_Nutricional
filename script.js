750dc7bc18caf804466b540c8e6ce3b6d2846b00
iagocavalcanti328 <iagoaraujo2025@gmail.com>
2025-11-07 10:43:24 -0300

    validação dos campos da primeira pagina

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ const buttonContinuar= document.querySelector("#buttonContinuar");
 const buttonVoltar = document.querySelector("#buttonVoltar");
 const buttonCancelar2= document.querySelector("#buttonCancelar2");
 
+// fazendo mudanças aqui jéssica (adicionando)
+const nomeImput = document.querySelector("#nome");
+const alturaInput = document.querySelector("#altura");
+const emailInput = document.querySelector("#email");
+const enderecoInput = document.querySelector("#endereco");
+const telefoneInput = document.querySelector("#telefone");
+const pesoInput = document.querySelector("#peso");
+
+
+
+
+
+
 buttonPreencherFicha.onclick = function (){
     modal1.showModal();
 }
@@ -15,7 +28,54 @@ buttonCancelar.onclick = function (){
     modal1.close();
 }
 
+
+
+
+
+
+
 buttonContinuar.onclick = function (){
+    
+    if (nomeInput.value.trim() === ""){
+        alert("por favor, preencha seu nome");
+        nomeImput.focus();
+        return;
+    }
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailInput.value.trim())){
+        alert("Por favor, insira um email válido");
+        emailInput.focus();
+        return;
+    }
+
+    const telefoneValidado = telefoneInput.value.replace(/\D/g, "");
+    if (telefoneValidado.lenght < 10) {
+        alert("por favor, insira um número válido (com ddd).");
+        telefoneInput.focus();
+        return;
+    }
+
+    if (pesoInput.value.trim() === "" || pesoInput.value <= 0){
+        alert("por favor, insira um peso válido (em kg).");
+        pesoInput.focus();
+        return;
+    }
+
+    if (enderecoInput.value.trim() === ""){
+        alert("por favor, preencha o seu endereço.");
+        enderecoInput.focus();
+        return;
+    }
+
+    if (alturaInput.value.trim() === "" || alturaInput.value <= 0){
+        alert("por favor, insira uma altura válida (em cm).");
+        alturaInput.focus;
+        return;
+    }
+    
+    modal1.close();
+
     modal2.showModal();
 }
 
